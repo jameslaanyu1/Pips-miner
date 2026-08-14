c588bf5b304935ddb1407b13bbd221134b27be80
jameslaanyu1 <Jameslaanyu@gmail.com>
2026-08-14 17:42:30 +0300

    Set velocity bot trailing distance to 100 pips

--- volatility_momentum_bot.py
+++ volatility_momentum_bot.py
@@ -5,7 +5,7 @@ from metaapi_cloud_sdk import MetaApi
 
 SYMBOL = "XAUUSD"
 LOT_SIZE = float(os.getenv("LOT_SIZE", "0.01"))
-TRAIL_PIPS = float(os.getenv("TRAIL_PIPS", "30"))
+TRAIL_PIPS = float(os.getenv("TRAIL_PIPS", "100"))
 def fmt_indicator(v):
     return f'{v:.2f}' if v is not None else 'N/A'
 
